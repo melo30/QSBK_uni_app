a8de6fe3de4ceecd20916d3016c98ca64cf355c1
陈诚 <281083409@qq.com>
2019-09-07 18:19:11 +0800

    tijiao

---
+++ pages/demo/demo.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<view class="view-box animated" hover-class="view-box-hover bounceIn">第一个view
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.view-box {
+		width: 200upx;
+		height: 200upx;
+		background: #007AFF;
+		color: #FFFFFF;
+		margin: 100upx;
+	}
+	.view-box-hover {
+		background: red;
+	}
+</style>
