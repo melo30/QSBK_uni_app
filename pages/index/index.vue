c5914244af356fa28a95e8b919632d4b93af8515
陈诚 <281083409@qq.com>
2019-09-18 17:24:13 +0800

    细节修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,28 +1,28 @@
 <template>
 	<view>
 		<view class="index-list">
-			<view class="index-list1 u-f-ac">
+			<view class="index-list1 u-f-ac u-f-jsb">
 				<view class="u-f-ac">
 						<image src="../../static/头像.png" mode="aspectFill" lazy-load></image>
-						昵称
+						陈诚
 				</view>
 				<view class="u-f-ac">
 					<view></view>+ 关注
 				</view>
 			</view>
-			<view class="index-list2">我是标题</view>
+			<view class="index-list2">这里是标题</view>
 			<view class="index-list3">
 				<image src="../../static/风景.png" mode="widthFix" lazy-load=""></image>
 			</view>
-			<view class="index-list4 u-f-ac">
+			<view class="index-list4 u-f-ac u-f-jsb">
 				<view class="u-f-ac">
-					<view class="u-f-ac"><image src="../../static/笑脸.png" mode="widthFix"></image>10</view>
-					<view class="u-f-ac"><image src="../../static/哭脸.png" mode="widthFix"></image>10</view>
+					<view class="u-f-ac"><image src="../../static/笑脸.png" mode="aspectFit"></image>10</view>
+					<view class="u-f-ac"><image src="../../static/哭脸.png" mode="aspectFit"></image>10</view>
 				</view>
 				
 				<view class="u-f-ac">
-					<view class="u-f-ac"><image src="../../static/评论.png" mode="widthFix"></image>10</view>
-					<view class="u-f-ac"><image src="../../static/点赞.png" mode="widthFix"></image>10</view>
+					<view class="u-f-ac"><image src="../../static/评论.png" mode="aspectFit"></image>10</view>
+					<view class="u-f-ac"><image src="../../static/点赞.png" mode="aspectFit"></image>10</view>
 				</view>
 			</view>
 		</view>
@@ -46,65 +46,49 @@
 </script>
 
 <style lang="scss">
-	.u-f, .u-f-ac,.u-f-ajc{
-		display: flex;
-	}
-	.u-f-ac , .u-f-ajc{
-		align-items: center;
-	}
-	.u-f-ajc {
-		justify-content: center;
-	}
 	.index-list {
-		padding: 10upx;
-		border-bottom: 1upx solid #999999;
-	}
-	.index-list1 {
-		justify-content: space-between;
+		padding: 20upx;
+		border-bottom: 1upx solid #E0E0E0;
 	}
 	.index-list1>view:first-child {
 		color: #999999;
-		font-size: 10upx;
+		font-size: 30upx;
 	}
 	.index-list1>view:first-child image{
-		width: 40upx;
-		height: 40upx;
+		width: 90upx;
+		height: 90upx;
 		border-radius: 100%;
-		margin-right: 10upx;
+		margin-right: 20upx;
 	}
 	.index-list1>view:last-child {
 		background: #F5f5f5;
-		border-radius: 5upx;
-		font-size: 10upx;
-		padding: 0 5upx;
-	}
-	.index-list1>view:last-child>view {
-		
+		border-radius: 10upx;
+		font-size: 30upx;
+		padding: 0 20upx;
 	}
 	.index-list2 {
-		padding-top: 5upx;
-		font-size: 12upx;
+		padding-top: 10upx;
+		font-size: 30upx;
 		color: #333333;
 	}
 	.index-list3 {
-		padding-top: 0upx;
+		padding-top: 20upx;
 	}
 	.index-list3>image {
 		width: 100%;
-		border-radius: 5upx;
+		border-radius: 10upx;
 	}
 	.index-list4 {
-		padding: 0upx 0;
-		justify-content: space-between;
+		padding: 10upx 0;
 	}
 	.index-list4>view {
 		color: #999999;
 	}
 	.index-list4>view>view {
-		font-size: 10upx;
+		font-size: 30upx;
 	}
 	.index-list4 image {
-		width: 10upx;
-		height: 10upx;
+		width: 30upx;
+		height: 30upx;
 	}
 </style>
